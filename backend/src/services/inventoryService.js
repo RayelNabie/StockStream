b13e031d0d62df249cfe2e7b3c1a9a5786902e75
Rayel <113995803+Rayel2002@users.noreply.github.com>
2025-01-22 14:36:10 +0100

    crreated code to generate an barcode

--- backend/src/services/inventoryService.js
+++ backend/src/services/inventoryService.js
@@ -25,24 +25,36 @@ export const createInventoryItem = async (data) => {
   }
 };
 
-export const assignBarcode = async () => {
-  //startcode
-  //jaartal
-  //maand
-  //dag
-  //uur
-  //Leading Zero's
-  //Loodsnummer
-  //bonnummer van de dag, auto increment
+// Lokaal array om receiptNumbers op te slaan
+const receiptNumbers = { lastReceiptNumber: 0 };
 
-  const createBarcode = [0, 0, 0, 0];
-  const date_time = Date();
-  console.log(date_time);
-  createBarcode.push(0,0,0,0)
-  //loodsnummer
+function getNextReceiptNumber() {
+  receiptNumbers.lastReceiptNumber =
+    (receiptNumbers.lastReceiptNumber + 1) % 10000; // Reset naar 0 na 9999
+  return receiptNumbers.lastReceiptNumber;
+}
 
+export const assignBarcode = async (warehouseNumber) => {
+  const startCode = "3910";
 
+  const now = new Date();
+  const year = now.getFullYear().toString().slice(-2);
+  const month = (now.getMonth() + 1).toString().padStart(2, "0");
+  const day = now.getDate().toString().padStart(2, "0");
+  const hour = now.getHours().toString().padStart(2, "0");
+  const minute = now.getMinutes().toString().padStart(2, "0");
+
+  const leadingZero = "00";
+
+  // const formattedWarehouseNumber = warehouseNumber.toString().padStart(2, "0");
+
+  const receiptNumber = getNextReceiptNumber().toString().padStart(4, "0");
   
+  // Combineer alle delen tot een barcode
+  const barcode = `${startCode}${year}${month}${day}${hour}${minute}${leadingZero}${receiptNumber}`;
+
+  console.log("Generated Barcode:", barcode);
+  return barcode;
 };
 
 export async function validateInventoryData(data) {
